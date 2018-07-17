dacb331cefefce578175d59db087f9d4708c4f57
WiseAngel <domiengustava@gmail.com>
2018-07-17 11:54:39 +0300

    upd: Написан код рендера блока apartment

--- app/js/components/apartment.js
+++ app/js/components/apartment.js
@@ -1,49 +1,221 @@
 let apartment = new Vue({
-  el: '#apartment',
-  data: {
-    title: 'Изучите планировки',
-    options: [{
-        option: '1-комнатный квартиры'
-      },
-      {
-        option: '2-комнатный квартиры'
-      },
-      {
-        option: '3-комнатный квартиры'
-      }
-    ],
+	el: '#apartment',
+	data: {
+		title: 'Изучите планировки',
+		offset: 0,
+		perPage: 4,
+		sortedLayouts: [],
+		options: [{
+				option: '1-комнатный квартиры',
+				rooms: 1,
+				isActive: true,
+			},
+			{
+				option: '2-комнатный квартиры',
+				rooms: 2,
+				isActive: false,
+			},
+			{
+				option: '3-комнатный квартиры',
+				rooms: 3,
+				isActive: false,
+			}
+		],
 
-    layouts: [{
-        area: '38,08',
-        value: '1 466 000',
-        img: 'img/_apartment-layout/apartment-layout__1room_1.jpg',
-        rooms: '1-комнатная квартира'
-      },
-      {
-        area: '38,08',
-        value: '1 466 000',
-        img: 'img/_apartment-layout/apartment-layout__1room_2.jpg',
-        rooms: '1-комнатная квартира'
-      },
-      {
-        area: '38,84',
-        value: '1 500 000',
-        img: 'img/_apartment-layout/apartment-layout__1room_3.jpg',
-        rooms: '1-комнатная квартира'
-      },
-      {
-        area: '39,53',
-        value: '1 522 000',
-        img: 'img/_apartment-layout/apartment-layout__1room_4.jpg',
-        rooms: '1-комнатная квартира'
-      },
-    ]
-  },
+		layouts: [{
+				area: '38,08',
+				value: '1 466 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_1.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '37,86',
+				value: '1 455 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_2.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '37,86',
+				value: '1 476 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_3.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_4.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,18',
+				value: '1 567 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_5.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,84',
+				value: '1 679 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_6.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,84',
+				value: '1 500 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_7.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,08',
+				value: '1 466 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_1.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '37,86',
+				value: '1 455 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_2.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '37,86',
+				value: '1 476 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_3.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_4.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,08',
+				value: '1 567 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_5.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,84',
+				value: '1 679 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_6.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '38,84',
+				value: '1 500 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_7.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '1-комнатная квартира',
+				rooms: 1
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 2
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 2
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 2
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 2
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 3
+			},
+			{
+				area: '39,53',
+				value: '1 522 000',
+				img: 'img/_apartment-layout/apartment-layout__1room_8.jpg',
+				alt: '2-комнатная квартира',
+				rooms: 3
+			},
+		],
 
-  methods: {
+		// sortedLayouts: [],
+	},
 
-  },
-  computed: {
+	methods: {
+		changeOffset(val) {
+			this.offset += val;
+			if (this.offset > this.sortedLayouts.length) {
+				this.offset = this.sortedLayouts.length;
+			} else if (this.offset < 0) {
+				this.offset = 0;
+			}
+		},
 
-  }
+		changeLayoutOptions(index) {
+			this.sortedLayouts = this.layouts.filter(filterByRooms);
+			this.offset = 0;
+			this.options.forEach(option => {
+				option.isActive = false;
+			});
+			this.options[index].isActive = true;
+
+			function filterByRooms(item) {
+				return item.rooms === index + 1;
+			};
+			console.log(this.sortedLayouts);
+			console.log(index);
+		},
+	},
+
+	computed: {
+		oneRoomsApartments: function () {
+			return this.layouts.filter(filterByOneRooms);
+
+			function filterByOneRooms(item) {
+				return item.rooms === 1;
+			};
+		},
+	},
+
 });
+apartment.sortedLayouts = apartment.oneRoomsApartments;
