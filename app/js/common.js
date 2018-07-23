76b6c3d910304d13b809574e0fd554a8013406e4
WiseAngel <domiengustava@gmail.com>
2018-07-23 15:42:20 +0300

    upd: Доработан калькулятор. Подсчет суммы кредита. Добавлены ограничения на вводимые числа. Изменены min max значения инпутов

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,7 @@
 $(function () {
-
+  /*
+  ** section__gallery
+  */
   $("#lightgallery").lightGallery({
     thumbnail: true,
     selector: '.gallery__item',
@@ -18,4 +20,30 @@ $(function () {
       }
     });
   })();
+  /*
+  ** /section__gallery
+  */
+
+  /*
+  ** section__calculator
+  */
+  $('.value-change__input_text').on('change', () => {
+    let arrayValues = section__calculator.calculatorValues;
+    for (let i = 0; i < arrayValues.length; i++) {
+      if (arrayValues[i].initialValue > arrayValues[i].maxValue) {
+        arrayValues[i].initialValue = arrayValues[i].maxValue;
+      }
+    }
+  });
+
+  let transformCreditAmountTotal = (() => {
+    $('.credit-amount__total').text((section__calculator.creditAmount).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.value-change__input').on('change', () => {
+      $('.credit-amount__total').text((section__calculator.creditAmount).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    });
+  })();
+  /*
+  ** /section__calculator
+  */
+
 });
--- app/js/components/calculator.js
+++ app/js/components/calculator.js
@@ -1,19 +1,17 @@
 let section__calculator = new Vue({
   el: '#calculator',
   data: {
-    foo: '',
-    fee: '',
-    faa: '',
     title: 'Калькулятор ипотеки',
     calculatorValues: [{
         initialValue: 1300000,
         minValue: 1300000,
         maxValue: 4000000,
-        step: 1000,
+        step: 10000,
         name: 'apartment-price',
         label: 'Стоимость квартиры',
         labelMinValue: '1 300 000',
         labelMaxValue: '4 000 000',
+        unit: '₽'
       },
       {
         initialValue: 10,
@@ -24,43 +22,74 @@ let section__calculator = new Vue({
         label: 'Срок кредита',
         labelMinValue: '1',
         labelMaxValue: '30',
+        unit: ''
       },
       {
         initialValue: 260000,
         minValue: '',
         maxValue: '',
-        step: 1000,
+        step: 100,
         name: 'initial-fee',
         label: 'Первоначальный взнос',
         labelMinValue: '20%',
         labelMaxValue: '100%',
+        unit: '₽'
       },
       {
-        initialValue: 6,
-        minValue: 1,
+        initialValue: 10,
+        minValue: 6,
         maxValue: 16,
         step: 1,
         name: 'loan-rate',
         label: 'Ставка по кредиту',
-        labelMinValue: '1',
+        labelMinValue: '6',
         labelMaxValue: '16',
+        unit: '%'
       }
-    ]
+    ],
+    creditAmountInCalculator: {
+      label: 'Сумма кредита:',
+      currency: '₽'
+    },
+    resultValules: [{
+      title: '',
+      value: '',
+      unit: ''
+    }]
   },
   methods: {
 
   },
-  /*   created() {
-      this.fee = this.calculatorValues[0].initialValue * 0.2;
-      this.foo = this.calculatorValues[0].initialValue;
-      this.faa = this.calculatorValues[2].minValue;
-
-    }, */
   computed: {
     initialFee() {
-      this.calculatorValues[2].minValue = this.calculatorValues[0].initialValue * 0.2;
-      this.calculatorValues[2].maxValue = this.calculatorValues[0].initialValue;
+      let price = this.calculatorValues[0].initialValue;
+
+      this.calculatorValues[2].minValue = price * 0.2;
+      this.calculatorValues[2].maxValue = price - 500000;
       this.calculatorValues[2].initialValue = this.calculatorValues[2].minValue;
+    },
+    creditAmount() {
+      return this.calculatorValues[0].initialValue - this.calculatorValues[2].initialValue;
+    },
+    declinationOfYear() {
+      let value = parseInt(this.calculatorValues[1].initialValue);
+      let spelling = '';
+      if (value === 1 || value === 21) {
+        spelling = 'Год';
+      } else if (2 <= value && value <= 4 || 22 <= value && value <= 24) {
+        spelling = 'Года';
+      } else {
+        spelling = 'Лет';
+      }
+      this.calculatorValues[1].unit = spelling;
+    },
+    foo() {
+      let arr = [];
+      this.calculatorValues.forEach((element, i) => {
+        i % 2 == 1 ? arr.push(element) : false;
+      });
+      arr.unshift(this.creditAmount);
+      return arr;
     }
   }
 });
